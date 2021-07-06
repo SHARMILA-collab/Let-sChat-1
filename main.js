47adef56ebd4f92da875117f8e1df535cb0292a1
SHARMILA-collab <83510211+SHARMILA-collab@users.noreply.github.com>
2021-07-06 17:58:33 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,18 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyCS3SK7hjMTYVI6_vJJ8O1eohV8NgSg8qg",
+    authDomain: "project-94-c8853.firebaseapp.com",
+    databaseURL: "https://project-94-c8853-default-rtdb.firebaseio.com",
+    projectId: "project-94-c8853",
+    storageBucket: "project-94-c8853.appspot.com",
+    messagingSenderId: "994158943421",
+    appId: "1:994158943421:web:4425e905b154b1f30e2bd4"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+
+  function addName() {
+      user_name = document.getElementById("user_name").value;
+      firebase.database().ref("/").child(user_name).update({
+          purpose: "adding user"
+      });
+  }
